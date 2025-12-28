0b80aa96e3188e287eceaa6fbce908c887255185
xitman327 <mcxitos327@gmail.com>
2025-12-28 04:04:19 +0200

    Add NodeRed exposure configuration to frpc.ini

--- run.sh
+++ run.sh
@@ -63,6 +63,14 @@ if [ ! -f /usr/src/frpc.ini ]; then
     echo "local_port = 445" >> /usr/src/frpc.ini
     echo "remote_port = 445" >> /usr/src/frpc.ini
 
+     echo "Adding NodeRed Exposure......."
+    echo "" >> /usr/src/frpc.ini
+    echo "[smb]" >> /usr/src/frpc.ini
+    echo "type = tcp" >> /usr/src/frpc.ini
+    echo "local_ip = 127.0.0.1 " >> /usr/src/frpc.ini
+    echo "local_port = 1880" >> /usr/src/frpc.ini
+    echo "remote_port = 1880" >> /usr/src/frpc.ini
+
     echo "Creating frpc.ini done"
 fi
 
